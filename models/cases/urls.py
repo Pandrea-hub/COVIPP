6398cf513f0fc9e73476293457945100b650afc7
Andrea <andiazf@unal.edu.co>
2021-07-24 12:38:26 -0500

    View case by person fixed

--- models/cases/urls.py
+++ models/cases/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^person/(?P<person_id>.+)/cases/$', CasesByPerson.as_view(), name='casesbyperson'),
     url(r'^person/(?P<person_id>.+)/symptomcases/$', CasesBySymptom.as_view(), name='casesbysymptom'),
     url(r'^person/(?P<person_id>.+)/contagioncases/$', CasesByContagion.as_view(), name='casesbycontagion'),
-    url(r'^case/(?P<case_id>.+)/casebyperson/(?P<person_id>.+)/person$', CaseByPerson.as_view(), name='casesbycontagion')
+    url(r'^case/(?P<case_id>.+)/casebyperson/(?P<person_id>.+)/person/$', CaseByPerson.as_view(), name='casesbycontagion')
 
 ]
 
