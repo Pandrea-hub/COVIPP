6233ef44137ab68c54d40a640ab1eeebd63606ba
Andrea <andiazf@unal.edu.co>
2021-07-25 16:06:14 -0500

    database view contagion and symptom fix

--- models/cases/views.py
+++ models/cases/views.py
@@ -26,7 +26,18 @@ class CaseContagionByPersonAndCase(generics.ListCreateAPIView):
     def get_queryset(self):
         person_id = self.kwargs['person_id']
         if 'cases_id' in self.kwargs:
-            cases_id = self.kwargs['case_id']
+            cases_id = self.kwargs['cases_id']
+            return Cases.objects.filter(person=person_id, pk=cases_id).distinct()
+        else:
+            return Cases.objects.filter(person=person_id).distinct()
+
+class CaseSymptomByPersonAndCase(generics.ListCreateAPIView):
+    serializer_class = CasesBySymptomSerializer
+
+    def get_queryset(self):
+        person_id = self.kwargs['person_id']
+        if 'cases_id' in self.kwargs:
+            cases_id = self.kwargs['cases_id']
             return Cases.objects.filter(person=person_id, pk=cases_id).distinct()
         else:
             return Cases.objects.filter(person=person_id).distinct()
